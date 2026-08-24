bce64bd5d66b4ef155ef28ffeae5e7ce2070e7a2
SivertEikrem <trevis.eikrem@gmail.com>
2026-08-24 23:47:21 +0200

    Create config.js for repository configuration
    Add configuration file for repository settings.

---
+++ docs/config.js
@@ -0,0 +1,4 @@
+/* Fast konfigurasjon for denne installasjonen - ikke sensitiv info,
+ * dette er allerede synlig i URL-en til det offentlige repoet. */
+const REPO_OWNER = "SivertEikrem";
+const REPO_NAME = "hertz-freerider-varsler";
