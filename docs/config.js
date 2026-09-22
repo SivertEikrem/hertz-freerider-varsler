0362486008cebbd307c538981fc4abab6ada7252
SivertEikrem <trevis.eikrem@gmail.com>
2026-09-22 18:04:35 +0200

    Delete docs directory

--- docs/config.js
+++
@@ -1,4 +0,0 @@
-/* Fast konfigurasjon for denne installasjonen - ikke sensitiv info,
- * dette er allerede synlig i URL-en til det offentlige repoet. */
-const REPO_OWNER = "SivertEikrem";
-const REPO_NAME = "hertz-freerider-varsler";
