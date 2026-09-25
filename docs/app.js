b2f0d7ac4a3fe9c1c688914d78cd385d8dd471cb
SivertEikrem <trevis.eikrem@gmail.com>
2026-09-25 13:27:58 +0200

    Update Telegram bot username in app.js

--- docs/app.js
+++ docs/app.js
@@ -7,7 +7,7 @@
  */
 
 // Brukernavnet på Telegram-boten din, UTEN @. Sett denne når boten er opprettet.
-const TELEGRAM_BOT_USERNAME = "DinFreeriderBot";
+const TELEGRAM_BOT_USERNAME = "Grtis_tur_bot";
 
 const $ = (id) => document.getElementById(id);
 
